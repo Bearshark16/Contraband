a587284ee11a53a6364b006975d097c9d6bee7f1
Bearshark16 <54946080+Bearshark16@users.noreply.github.com>
2020-02-05 14:46:19 +0100

    Skrev logiken

--- Car.cs
+++ Car.cs
@@ -12,6 +12,8 @@ namespace Contraband
         public int contrabandAmount;
         public bool alreadyChecked;
 
+        private int x;
+
         public bool Examine()
         {
             alreadyChecked = true;
@@ -22,7 +24,42 @@ namespace Contraband
             }
             else
             {
-                return true;
+                if (contrabandAmount == 1)
+                {
+                    x = generator.Next(1, 26);
+                    if (x <= 10)
+                        return false;
+                    else
+                        return true;
+                }
+                else if (contrabandAmount == 2)
+                {
+                    x = generator.Next(5, 21);
+                    if (x <= 10)
+                        return false;
+                    else
+                        return true;
+                }
+                else if (contrabandAmount == 3)
+                {
+                    x = generator.Next(10, 16);
+                    if (x <= 10)
+                        return false;
+                    else
+                        return true;
+                }
+                else if (contrabandAmount == 4)
+                {
+                    x = generator.Next(15, 16);
+                    if (x <= 10)
+                        return false;
+                    else
+                        return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Contraband
 {
@@ -6,7 +7,74 @@ namespace Contraband
     {
         static void Main(string[] args)
         {
-            
+            Random random = new Random();
+
+            List<Car> cars = new List<Car>();
+
+            string input = "";
+            int x = 0;
+            int y = 0;
+            int z = 0;
+
+            bool result;
+
+            Console.WriteLine("Hur många bilar ska skapa?\n");
+            Input(out input, out x);
+
+            for (int i = 0; i < x; i++)
+            {
+                y = random.Next(1, 11);
+
+                if (y >= 5)
+                {
+                    cars.Add(new CleanCar());
+                }
+                else if (y < 5)
+                {
+                    cars.Add(new ContrabandCar());
+                }
+            }
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Vilken bil vill du titta på?\n");
+                Input(out input, out x);
+                while (x > cars.Count || x <= 0)
+                {
+                    Input(out input, out x);
+                }
+                if (cars[x-1].alreadyChecked)
+                {
+                    Console.WriteLine("Den bilen har du redan tittat på!");
+                }
+                else
+                {
+                    result = cars[x-1].Examine();
+
+                    if (result)
+                    {
+                        Console.WriteLine("Det finns contraband i den här bilen!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Den här bilen är ren");
+                    }
+                }
+
+                Console.ReadLine();
+            }
+        }
+
+        private static void Input(out string input, out int x)
+        {
+            Console.Write("> ");
+            input = Console.ReadLine();
+            while (!int.TryParse(input, out x) && x <= 0)
+            {
+                Console.Write("> ");
+                input = Console.ReadLine();
+            }
         }
     }
 }
